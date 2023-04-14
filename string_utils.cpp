12708f1f4e6065fd85bb6000b6175f22000c34dc
Artem Kliminskyi <graphene64@gmail.com>
2023-04-14 16:41:59 +0300

    Add has_errors and unite_errors to the string_utils file.

--- string_utils.cpp
+++ string_utils.cpp
@@ -12,3 +12,28 @@ void trim(std::string& string, const std::string& redunant) {
     trim_right(string);
     trim_left(string);
 }
+
+[[nodiscard]] bool has_errors(const std::vector<std::optional<std::string>>& errors) {
+    for (const auto& error : errors) {
+        if (error.has_value()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+[[nodiscard]] std::string unite_errors(const std::vector<std::optional<std::string>>& errors) {
+    std::string result;
+
+    for (const auto& error : errors) {
+        if (!error.has_value()) {
+            continue;
+        }
+
+        result += *error;
+        result += "<br/>";
+    }
+
+    return result;
+}
--- string_utils.hpp
+++ string_utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <optional>
 
 [[nodiscard]] inline bool is_english_letter(char c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
@@ -15,3 +17,6 @@ const std::string whitespaces {" \t\n\r\f\v"};
 void trim_left(std::string& string, const std::string& redunant = whitespaces);
 void trim_right(std::string& string, const std::string& redunant = whitespaces);
 void trim(std::string& string, const std::string& redunant = whitespaces);
+
+[[nodiscard]] bool has_errors(const std::vector<std::optional<std::string>>& errors);
+[[nodiscard]] std::string unite_errors(const std::vector<std::optional<std::string>>& errors);
